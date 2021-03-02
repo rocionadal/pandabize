4e876fe29a6dba4641de7133b85e41be40366b71
rocionadal <rocioonadal@gmail.com>
2021-03-02 21:06:03 +0100

    color and size selector base

---
+++ app/javascript/packs/components/colorSelector/colorSelector.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './ColorSelector.module.scss';
+
+const ColorSelector = (props) => {
+
+
+
+    return (
+        <div className={classes.ColorSelector}><h1>Color selector</h1></div>
+    )
+}
+
+export default ColorSelector;
---
+++ app/javascript/packs/components/sizeSelector/sizeSelector.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './SizeSelector.module.scss';
+
+const SizeSelector = (props) => {
+
+
+
+    return (
+        <div className={classes.SizeSelector}><h1>Size selector</h1></div>
+    )
+}
+
+export default SizeSelector;
